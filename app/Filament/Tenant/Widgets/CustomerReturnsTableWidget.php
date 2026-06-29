3f4c872281c4fa80ed02816162f100553ca0d3b3
Liviutexora <loyaltyclub.eu@gmail.com>
2026-06-29 11:36:33 +0300

    Finalizare structură tabel Reminder clienți

--- app/Filament/Tenant/Widgets/CustomerReturnsTableWidget.php
+++ app/Filament/Tenant/Widgets/CustomerReturnsTableWidget.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Tenant\Widgets;
 
 use App\Filament\Tenant\Resources\ClientResource;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
 
@@ -12,9 +13,31 @@ class CustomerReturnsTableWidget extends BaseWidget
 
     protected static ?string $heading = null;
 
+    protected function getTableHeading(): string|\Illuminate\Contracts\Support\Htmlable|null
+    {
+        return __('Clienți programați pentru reminder');
+    }
+
     public function table(Table $table): Table
     {
         // Reuse the exact same Table Builder used by Clients page.
-        return ClientResource::table($table);
+        $table = ClientResource::table($table);
+
+        $columns = array_map(function ($column) {
+            return match ($column->getName()) {
+                'total_bookings'   => $column->label(__('Reminder')),
+                'favourite_service' => $column->label(__('Serviciu')),
+                'total_spent'      => $column->label(__('Status')),
+                default            => $column,
+            };
+        }, $table->getColumns());
+
+        $columns[] = TextColumn::make('actions_placeholder')
+            ->label(__('Acțiuni'))
+            ->state(static fn () => __('În curând'));
+
+        return $table
+            ->columns($columns)
+            ->actions([]);
     }
 }
